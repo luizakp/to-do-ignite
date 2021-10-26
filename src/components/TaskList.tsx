3b9d4623cf954f82ca0af0b299a2caaf3691a500
luizapuccinelli <luizapuccinelli06@gmail.com>
2021-10-26 20:54:34 -0300

    todo using react hooks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,16 +15,38 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    if (!newTaskTitle === false){
+        const newTask:Task = {
+          id: Math.floor(Math.random() * 100),
+          title: newTaskTitle,
+          isComplete: false,
+        }
 
+        setTasks((tasks => [...tasks, newTask]))
+
+        setNewTaskTitle("")
+      }
+    }
+    
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if(task.id === id){
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        };
+      }
+      else {
+        return task;
+      }
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
+
+  console.log(tasks)
 
   return (
     <section className="task-list container">
